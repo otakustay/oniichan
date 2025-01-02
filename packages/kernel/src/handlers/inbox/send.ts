7ee0a5d9fa91ba875d61230946777b6175100f32
otakustay <otakustay@gmail.com>
2025-01-02 11:39:44 +0800

    feat: stop tool call automatically in certain cases

--- packages/kernel/src/handlers/inbox/send.ts
+++ packages/kernel/src/handlers/inbox/send.ts
@@ -1,11 +1,14 @@
-import {ChatInputPayload, ModelToolResponse} from '@oniichan/shared/model';
+import deepEqual from 'fast-deep-equal';
+import {over} from '@otakustay/async-iterator';
+import {ChatInputPayload, ModelTextResponse, ModelToolResponse} from '@oniichan/shared/model';
 import {Message, MessageReference} from '@oniichan/shared/inbox';
 import {FunctionUsageTelemetry} from '@oniichan/storage/telemetry';
 import {now, stringifyError} from '@oniichan/shared/string';
 import {newUuid} from '@oniichan/shared/id';
+import {ModelChatOptions} from '../../editor/model';
 import {RequestHandler} from '../handler';
 import {store} from './store';
-import {tools, ToolImplement} from './tool';
+import {ToolImplement} from './tool';
 
 interface TextMessageBody {
     type: 'text';
@@ -38,11 +41,6 @@ interface ToolCallState {
     current: ModelToolResponse | null;
 }
 
-interface RequestOptions {
-    messages: ChatInputPayload[];
-    replyUuid: string;
-}
-
 interface PathArguments {
     path: string;
 }
@@ -66,16 +64,43 @@ function toolCallToReference(toolCall: ModelToolResponse): MessageReference {
     }
 }
 
+function threadMessageToInputPayload(message: Message): ChatInputPayload {
+    return {
+        role: message.sender,
+        content: message.content,
+    };
+}
+
 export class InboxSendMessageHandler extends RequestHandler<InboxSendMessageRequest, InboxSendMessageResponse> {
     static action = 'inboxSendMessage' as const;
 
     private telemetry: FunctionUsageTelemetry = new FunctionUsageTelemetry(this.getTaskId(), 'inboxSendMessage');
 
+    private readonly messages: ChatInputPayload[] = [];
+
+    private threadUuid = '';
+
+    private replyUuid = newUuid();
+
+    private enableTool = true;
+
+    private readonly toolCallLimit = 10;
+
+    private tool: ToolImplement | null = null;
+
     async *handleRequest(payload: InboxSendMessageRequest): AsyncIterable<InboxSendMessageResponse> {
-        const {logger} = this.context;
+        const {logger, editorHost} = this.context;
         logger.info('Start', payload);
+
+        this.tool = new ToolImplement(editorHost);
+        this.threadUuid = payload.threadUuid;
         const thread = store.getThreadByUuid(payload.threadUuid);
+        if (thread) {
+            // Messages are latest-on-top in thread
+            this.messages.unshift(...thread.messages.map(threadMessageToInputPayload));
+        }
         this.telemetry.setTelemetryData('mode', thread ? 'new' : 'reply');
+
         try {
             yield* this.telemetry.spyStreaming(() => this.chat(payload));
             logger.info('Finish');
@@ -85,60 +110,131 @@ export class InboxSendMessageHandler extends RequestHandler<InboxSendMessageRequ
         }
     }
 
-    private async *requestModel(options: RequestOptions): AsyncIterable<InboxSendMessageResponse> {
+    private persistToolChunk(chunk: ModelToolResponse): InboxSendMessageResponse {
         const {logger} = this.context;
-        logger.trace('RequestModelStart', options);
-        const {messages, replyUuid} = options;
+        const reference = toolCallToReference(chunk);
+        logger.trace(
+            'AddReference',
+            {threadUuid: this.threadUuid, messageUuid: this.replyUuid, reference: chunk}
+        );
+        // Broadcast tool usage
+        store.addReference(this.threadUuid, this.replyUuid, reference);
+        this.updateInboxThreadList(store.dump());
+        return {uuid: this.replyUuid, type: 'reference', value: reference};
+    }
+
+    private persistTextChunk(chunk: ModelTextResponse): InboxSendMessageResponse {
+        const {logger} = this.context;
+        logger.trace(
+            'AppendMessage',
+            {threadUuid: this.threadUuid, messageUuid: this.replyUuid, chunk: chunk}
+        );
+        // We update the store but don't broadcast to all views on streaming
+        store.appendMessage(this.threadUuid, this.replyUuid, chunk.content);
+        return {uuid: this.replyUuid, type: 'text', value: chunk.content};
+    }
+
+    private hasSameToolCall(toolCall: ModelToolResponse): boolean {
+        const isSame = (message: ChatInputPayload) => {
+            if (message.role !== 'assistant') {
+                return false;
+            }
+            if (!message.toolCall) {
+                return false;
+            }
+
+            return message.toolCall.functionName === toolCall.name
+                && deepEqual(message.toolCall.arguments, toolCall.arguments);
+        };
+        return this.messages.some(isSame);
+    }
+
+    private reachesToolCallLimit(): boolean {
+        const calls = this.messages.filter(message => message.role === 'assistant').filter(v => !!v.toolCall);
+
+        return calls.length >= this.toolCallLimit;
+    }
+
+    private async *handleToolCall(toolCall: ModelToolResponse): AsyncIterable<InboxSendMessageResponse> {
+        const {logger} = this.context;
+
+        // Stop if the same tool is called with the same arguments, this is possibily a bug behavior of LLM.
+        if (this.hasSameToolCall(toolCall)) {
+            logger.info('ToolCallDuplicated', toolCall);
+            this.enableTool = false;
+            yield* this.requestModel();
+            return;
+        }
+
+        if (this.reachesToolCallLimit()) {
+            logger.info('ToolCallLimitReached', toolCall);
+            this.enableTool = false;
+            yield* this.requestModel();
+            return;
+        }
+
+        logger.trace('HandleToolCallStart', toolCall);
+        const implement = new ToolImplement(this.context.editorHost);
+        const result = await implement.callTool(toolCall.name, toolCall.arguments);
+        logger.trace('HandleToolCallFinish', {...toolCall, result});
+        this.messages.push(
+            {
+                role: 'assistant',
+                content: '',
+                toolCall: {
+                    id: toolCall.id,
+                    functionName: toolCall.name,
+                    arguments: toolCall.arguments,
+                },
+            },
+            {
+                role: 'tool',
+                callId: toolCall.id,
+                content: result,
+            }
+        );
+        yield* this.requestModel();
+    }
+
+    private async *requestModel(): AsyncIterable<InboxSendMessageResponse> {
+        const {logger} = this.context;
+        logger.trace('RequestModelStart', {threadUuid: this.threadUuid, messages: this.messages});
         const {editorHost} = this.context;
         const model = editorHost.getModelAccess(this.getTaskId());
         const modelTelemetry = this.telemetry.createModelTelemetry(this.getTaskId());
-        const tool: ToolCallState = {
-            current: null,
+        const tool: ToolCallState = {current: null};
+        const options: ModelChatOptions = {
+            tools: this.enableTool ? (this.tool?.getBuiltinTools() ?? []) : [],
+            messages: this.messages,
+            telemetry: modelTelemetry,
         };
-        for await (const chunk of model.chatStreaming({tools, messages, telemetry: modelTelemetry})) {
+        for await (const chunk of model.chatStreaming(options)) {
             logger.trace('RequestModelChunk', chunk);
             if (chunk.type === 'tool') {
                 tool.current = chunk;
-                yield {uuid: replyUuid, type: 'reference', value: toolCallToReference(chunk)};
+                // Don't add reference of duplicated tool call to store, this will be handled in `handleToolCall` later
+                if (!this.hasSameToolCall(chunk)) {
+                    yield this.persistToolChunk(chunk);
+                }
             }
             else {
-                yield {uuid: replyUuid, type: 'text', value: chunk.content};
+                yield this.persistTextChunk(chunk);
             }
         }
         logger.trace('RequestModelFinish');
 
         if (tool.current) {
-            logger.trace('HandleToolCallStart', tool.current);
-            const implement = new ToolImplement(this.context.editorHost);
-            const result = await implement.callTool(tool.current.name, tool.current.arguments);
-            logger.trace('HandleToolCallFinish', {...tool.current, result});
-            const nextMessages: ChatInputPayload[] = [
-                ...messages,
-                {
-                    role: 'assistant',
-                    content: '',
-                    toolCall: {
-                        id: tool.current.id,
-                        functionName: tool.current.name,
-                        arguments: tool.current.arguments,
-                    },
-                },
-                {
-                    role: 'tool',
-                    callId: tool.current.id,
-                    content: result,
-                },
-            ];
-            yield* this.requestModel({messages: nextMessages, replyUuid});
+            yield* this.handleToolCall(tool.current);
         }
     }
 
     private async *chat(payload: InboxSendMessageRequest) {
         const {logger} = this.context;
         logger.trace('AddNewMessageToStore', payload);
+        this.messages.push({role: 'user', content: payload.body.content});
         // Add a user message to thread (or create a new thread)
         store.addNewMessageToThreadList(
-            payload.threadUuid,
+            this.threadUuid,
             {
                 uuid: payload.uuid,
                 sender: 'user',
@@ -148,57 +244,22 @@ export class InboxSendMessageHandler extends RequestHandler<InboxSendMessageRequ
                 status: 'read',
             }
         );
-        logger.trace('PushStoreUpdate', payload);
+        logger.trace('PushStoreUpdate');
         this.updateInboxThreadList(store.dump());
 
-        const thread = store.getThreadByUuid(payload.threadUuid);
-
-        if (!thread) {
-            // Not possible
-            throw new Error(`Cannot find thread ${payload.threadUuid} after it is created`);
-        }
-
-        const toMessagePayload = (message: Message): ChatInputPayload => {
-            return {
-                role: message.sender,
-                content: message.content,
-            };
-        };
-        // Messages are latest-on-top in thread
-        const messages = thread.messages.map(toMessagePayload).reverse();
-        const replyUuid = newUuid();
         try {
-            for await (const chunk of this.requestModel({messages, replyUuid})) {
-                if (chunk.type === 'text') {
-                    logger.trace(
-                        'AppendMessage',
-                        {threadUuid: payload.threadUuid, messageUuid: replyUuid, chunk: chunk.value}
-                    );
-                    // We update the store but don't broadcast to all views on streaming
-                    store.appendMessage(payload.threadUuid, replyUuid, chunk.value);
-                }
-                else {
-                    logger.trace(
-                        'AddReference',
-                        {threadUuid: payload.threadUuid, messageUuid: replyUuid, reference: chunk.value}
-                    );
-                    // Broadcast tool usage
-                    store.addReference(payload.threadUuid, replyUuid, chunk.value);
-                    this.updateInboxThreadList(store.dump());
-                }
-
-                yield {type: 'value', value: chunk} as const;
-            }
+            yield* over(this.requestModel()).map(v => ({type: 'value', value: v} as const));
         }
         catch (ex) {
-            store.setMessageError(payload.threadUuid, replyUuid, stringifyError(ex));
+            store.setMessageError(this.threadUuid, this.replyUuid, stringifyError(ex));
             throw ex;
         }
-
-        // Broadcast update when message is fully generated
-        logger.trace('MarkMessageUnread', {threadUuid: payload.threadUuid, messageUuid: replyUuid});
-        store.markStatus(payload.threadUuid, replyUuid, 'unread');
-        logger.trace('PushStoreUpdate', payload);
-        this.updateInboxThreadList(store.dump());
+        finally {
+            // Broadcast update when message is fully generated
+            logger.trace('MarkMessageUnread', {threadUuid: this.threadUuid, messageUuid: this.replyUuid});
+            store.markStatus(this.threadUuid, this.replyUuid, 'unread');
+            logger.trace('PushStoreUpdate');
+            this.updateInboxThreadList(store.dump());
+        }
     }
 }
--- packages/kernel/src/handlers/inbox/tool.ts
+++ packages/kernel/src/handlers/inbox/tool.ts
@@ -1,8 +1,9 @@
 import path from 'node:path';
 import {ChatToolPayload} from '@oniichan/shared/model';
+import {stringifyError} from '@oniichan/shared/string';
 import {EditorHost} from '../../editor';
 
-export const tools: ChatToolPayload[] = [
+const tools: ChatToolPayload[] = [
     {
         name: 'readDirectory',
         description: 'Read the file and child directories in a directory',
@@ -44,31 +45,45 @@ export class ToolImplement {
         this.editorHost = editorHost;
     }
 
+    getBuiltinTools() {
+        return tools;
+    }
+
     async callTool(name: string, args: unknown): Promise<unknown> {
         if (name === 'readDirectory') {
             const arg = args as PathArguments;
             const workspace = this.editorHost.getWorkspace();
-            const root = await workspace.getRoot();
-            if (root) {
-                const children = await workspace.readDirectory(path.join(root, arg.path));
-                return children.length > 200 ? children.slice(0, 200) : children;
+            try {
+                const root = await workspace.getRoot();
+                if (root) {
+                    const children = await workspace.readDirectory(path.join(root, arg.path));
+                    return children.length > 200 ? children.slice(0, 200) : children;
+                }
+                return [];
             }
-            return [];
+            catch (ex) {
+                return `Unsable to read directory ${arg.path}: ${stringifyError(ex)}`;
+            }
         }
         if (name === 'readFile') {
             const arg = args as PathArguments;
             const workspace = this.editorHost.getWorkspace();
-            const root = await workspace.getRoot();
-            if (root) {
-                const content = await workspace.readFile(path.join(root, arg.path));
+            try {
+                const root = await workspace.getRoot();
+                if (root) {
+                    const content = await workspace.readFile(path.join(root, arg.path));
 
-                if (content.length > 30000) {
-                    return `Unable to read ${arg.path}: This file is too large`;
+                    if (content.length > 30000) {
+                        return `Unable to read file ${arg.path}: This file is too large`;
+                    }
+
+                    return content;
                 }
-
-                return content;
+                return null;
             }
-            return null;
+            catch (ex) {
+                return `Unsable to read file ${arg.path}: ${stringifyError(ex)}`;
+            }
         }
         throw new Error(`Unknown tool "${name}"`);
     }
