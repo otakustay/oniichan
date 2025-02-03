5a2395f45639302b3823b86ec3352df4238047ab
otakustay <otakustay@gmail.com>
2025-02-03 21:00:38 +0800

    fix: migration results incorrect config value

--- packages/vscode/src/migration/utils.ts
+++ packages/vscode/src/migration/utils.ts
@@ -1,8 +1,7 @@
 import {ConfigurationTarget, workspace} from 'vscode';
 
 export function deleteConfiguration(section: string) {
-    // TODO: Should be `undefined`
-    workspace.getConfiguration().update(section, null, ConfigurationTarget.Global);
-    workspace.getConfiguration().update(section, null, ConfigurationTarget.Workspace);
-    workspace.getConfiguration().update(section, null, ConfigurationTarget.WorkspaceFolder);
+    workspace.getConfiguration().update(section, undefined, ConfigurationTarget.Global);
+    workspace.getConfiguration().update(section, undefined, ConfigurationTarget.Workspace);
+    workspace.getConfiguration().update(section, undefined, ConfigurationTarget.WorkspaceFolder);
 }
