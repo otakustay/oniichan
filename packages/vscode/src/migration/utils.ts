a993a35a59a111f2e5246142cf93e67be6914891
otakustay <otakustay@gmail.com>
2025-02-10 16:41:18 +0800

    fix: only migrate global config to prevent auto creation of settings.json

--- packages/vscode/src/migration/utils.ts
+++ packages/vscode/src/migration/utils.ts
@@ -1,7 +1,11 @@
 import {ConfigurationTarget, workspace} from 'vscode';
 
 export function deleteConfiguration(section: string) {
-    workspace.getConfiguration().update(section, undefined, ConfigurationTarget.Global);
-    workspace.getConfiguration().update(section, undefined, ConfigurationTarget.Workspace);
-    workspace.getConfiguration().update(section, undefined, ConfigurationTarget.WorkspaceFolder);
+    const configuration = workspace.getConfiguration();
+
+    if (!configuration.has(section)) {
+        return;
+    }
+
+    configuration.update(section, undefined, ConfigurationTarget.Global);
 }
