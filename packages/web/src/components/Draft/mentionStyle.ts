821bc968b21b55eadd6fe4e8b5195f6640205d30
otakustay <otakustay@gmail.com>
2025-04-04 21:34:13 +0800

    fix: metion popover should have default background in dark theme

--- packages/web/src/components/Draft/mentionStyle.ts
+++ packages/web/src/components/Draft/mentionStyle.ts
@@ -11,6 +11,7 @@ const style: MentionsInputStyle = {
             padding: '0 .5em',
             lineHeight: '2',
             borderBottom: '1px solid var(--color-default-border)',
+            backgroundColor: 'var(--color-default-background)',
             color: 'var(--color-default-foreground)',
 
             '&focused': {
